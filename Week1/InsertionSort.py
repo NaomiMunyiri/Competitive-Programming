9040f388e477222c07b86aad7e0954516a79e9e9
NaomiMunyiri <naomi.munyiri11@gmail.com>
2022-10-25 21:06:02 +0300

    Insertion Sort

---
+++ Week1/InsertionSort.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    target=arr[-1] #fetches last element if array
+    i=n-1 #selects from last indes
+    while i > 0 and arr[i-1]>target: #traverses backwards
+       arr[i]=arr[i-1] #copies previous element to current index
+       print(*arr) #prints array
+       i=i-1
+    arr[i]=target #assigning target to corresponding index
+    print(*arr)
+    
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
